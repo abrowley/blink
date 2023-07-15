f90e01b8ba91df7db3117d0ea11083d42216c8be
Alex Rowley <alex.rowley@gmail.com>
2023-07-15 21:34:08 +0100

    refactored

--- src/main.c
+++ src/main.c
@@ -1,13 +1,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
-void blink() {
-    PORTB ^= (1 << PB0);
-}
+#include "../lib/blink.h"
 
 int main(void)
 {
-    /* Sets PORT B.5 as an output */
     DDRB |= (1 << PB0);
     while(1){
         blink();
