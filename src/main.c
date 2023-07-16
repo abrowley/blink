dd1c92e7e88ad6ad81a0b1b653cffa07d7e0adcf
Alex Rowley <alex.rowley@gmail.com>
2023-07-16 10:57:32 +0100

    change delay

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "../lib/blink.h"
+#include <blink.h>
 
 int main(void)
 {
     DDRB |= (1 << PB0);
     while(1){
         blink();
-        _delay_ms(100);
+        _delay_ms(1000);
     }
 }
